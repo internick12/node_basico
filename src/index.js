fa3aa3dedefdf8beaaf7619b2614d88710178c78
Jose Garcia <j_t_g_l@yahoo.com.mx>
2021-09-17 14:02:47 -0500

    hhh

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+require('dotenv').config();
+
+const port = process.env.PORT;
+
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(require('./routes/index'));
+
+app.use(express.static(path.join(__dirname , 'public')));
+
+app.listen(port, () => {
+    console.log(`Corriendo en el puerto: ${ port }`);
+});
